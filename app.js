d354bc779c44828bb6143ab49fefb7d31e5c9fd4
happyhermann <carrick035@gmail.com>
2022-05-05 15:59:39 +0900

    implemented
    progress in 10%

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-// const 요청url = "https://eulsoo.github.io/list.json"
-// const 요청객체 = new XMLHttpRequest();
-// 요청객체.open('GET', 요청url);
-// 요청객체.responseType = 'json';
-// 요청객체.send();
-// 요청객체.onload = () => {
-//     const obj = 요청객체.response;
-//     할일(obj);
-// }
+// // const 요청url = "https://eulsoo.github.io/list.json"
+// // const 요청객체 = new XMLHttpRequest();
+// // 요청객체.open('GET', 요청url);
+// // 요청객체.responseType = 'json';
+// // 요청객체.send();
+// // 요청객체.onload = () => {
+// //     const obj = 요청객체.response;
+// //     할일(obj);
+// // }
 
 
-fetch("https://eulsoo.github.io/list.json")
-.then(function (res) {
- return res.json()
-}).then(function(obj) {
-    console.log(obj);
-})
+// fetch("https://eulsoo.github.io/list.json")
+// .then(function (res) {
+//  return res.json()
+// }).then(function(obj) {
+//     console.log(obj);
+// })
 
  
-function 할일 (obj) {
-    const UL = document.querySelector('.list')
+// function 할일 (obj) {
+//     const UL = document.querySelector('.list')
 
-    for (let i = 0; i < obj.length; i++) {
-        const li = document.createElement('LI');
-        li.textContent = obj[i].item
-        UL.appendChild(li);
+//     for (let i = 0; i < obj.length; i++) {
+//         const li = document.createElement('LI');
+//         li.textContent = obj[i].item
+//         UL.appendChild(li);
         
 
 
-    }
+//     }
 
  
-};
+// };
 
 
 
