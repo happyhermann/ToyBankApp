43f461593ce8a6df5461d182852255484724a332
happyhermann <carrick035@gmail.com>
2022-05-05 21:51:36 +0900

    progress

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+
+ // import Swiper JS
+ import Swiper from 'swiper';
+ // import Swiper styles
+ import 'swiper/css';
+
+ const swiper = new Swiper(...);
+
+  // core version + navigation, pagination modules:
+  import Swiper, { Navigation, Pagination } from 'swiper';
+  // import Swiper and modules styles
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+  // init Swiper:
+  const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    ...
+  });
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
 // // const 요청url = "https://eulsoo.github.io/list.json"
 // // const 요청객체 = new XMLHttpRequest();
 // // 요청객체.open('GET', 요청url);
@@ -34,3 +82,6 @@
 
 
 
+
+
+ 
