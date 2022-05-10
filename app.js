9e1a20e0e4a3bc7a3f80f3b07e17c5553108ab6c
happyhermann <carrick035@gmail.com>
2022-05-10 20:23:29 +0900

    latest

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
 
 
-function readJson(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
+// function readJson(file, callback) {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function () {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
 
-        }
-    }
-    rawFile.send(null);
+//         }
+//     }
+//     rawFile.send(null);
 
-}
-
-readJson("./data.json", function (text) {
-    var Data = JSON.parse(text);
-    console.log(Data);
-    // 거래(Data);
-    TodayList(Data) 
-    yesterDayList(Data)
-    historyList(Data);
-
-});
- 
- 
-s
-
-// 강사님 방식  
-
-// const 요청url = "https://eulsoo.github.io/list.json"
-// const 요청객체 = new XMLHttpRequest();
-// 요청객체.open('GET', 요청url);
-// 요청객체.responseType = 'json';
-// 요청객체.send();
-// 요청객체.onload = () => {
-//     const obj = 요청객체.response;
-//     할일(obj);
 // }
 
+// readJson("./data.json", function (text) {
+//     var Data = JSON.parse(text);
+//     console.log(Data);
+//     // 거래(Data);
+//     TodayList(Data) 
+//     yesterDayList(Data)
+//     historyList(Data);
+
+// });
+ 
+ 
+ 
+  
+
+const 요청url = "https://jytrack64.github.io/data.json"
+const 요청객체 = new XMLHttpRequest();
+요청객체.open('GET', 요청url);
+요청객체.responseType = 'json';
+요청객체.send();
+요청객체.onload = () => {
+    const obj = 요청객체.response;
+    List(obj);
+}
 
 
-// fetch("https://eulsoo.github.io/list.json")
-// .then(function (res) {
-//  return res.json()
-// }).then(function(obj) {
-//     console.log(obj);
-// })
+
+fetch("https://jytrack64.github.io/data.json")
+.then(function (res) {
+ return res.json()
+}).then(function(obj) {
+    console.log(obj);
+})
 
 
 
@@ -81,22 +80,22 @@ const history = document.querySelector(".history");
 // };
 
 
-function historyList(Data) {
+function List(obj) {
 
     const UL = document.querySelector('.list')
     const spendingText = document.querySelector('.history_spending')
     let sum = 0;
 
-    for (let i = 0; i < Data.length; i++) {
+    for (let i = 0; i < obj.length; i++) {
 
 
         const creLi = document.createElement('LI');
         creLi.className = "creLi";
         const span1 = document.createElement("SPAN");
         const span2 = document.createElement("SPAN");
-        span1.textContent = Data[i].item
-        // span2.textContent = Data[i].price
-        let price = Data[i].price;
+        span1.textContent = obj[i].item
+        // span2.textContent = obj[i].price
+        let price = obj[i].price;
         creLi.style.borderBottom = "1px solid #dcdcdc";
         creLi.style.padding = "3px 17px"
         creLi.style.fontSize = "18px"
@@ -106,18 +105,18 @@ function historyList(Data) {
         creLi.appendChild(span1);
         creLi.appendChild(span2);
 
-        if(Data[i].inOut === "in" )  {
+        if(obj[i].inOut === "in" )  {
             span2.textContent = `+ ${price}원`
             span2.style.color = "#008000";
 
           }  
-          else if (Data[i].inOut ===     "out") {
+          else if (obj[i].inOut ===     "out") {
             span2.textContent = `- ${price}원`
           }
 
-        if (Data[i].inOut === "in") {
+        if (obj[i].inOut === "in") {
             sum = sum + price;
-        } if (Data[i].inOut === "out") {
+        } if (obj[i].inOut === "out") {
             sum = sum - price;
         }
     
@@ -127,105 +126,25 @@ function historyList(Data) {
 
 
 
-function TodayList(Data) {
-       
-    const UL = document.querySelector('.list')
-    const spendingText = document.querySelector('.history_spending')
-    let sum = 0;
-
-    for (let i = 0; i < Data.length; i++) {
-
-        
-        const creLi = document.createElement('LI');
-        creLi.className = "creLi";
-        const span1 = document.createElement("SPAN");
-        const span2 = document.createElement("SPAN");
-        span1.textContent = Data[i].item
-        // span2.textContent = Data[i].price
-        let price = Data[i].price;
-        creLi.style.borderBottom = "1px solid #dcdcdc";
-        creLi.style.padding = "3px 17px"
-        creLi.style.fontSize = "18px"
-        creLi.style.fontWeight = "300";
-        creLi.style.lineHeight = "27px";
-        history.appendChild(creLi);
-        creLi.appendChild(span1);
-        creLi.appendChild(span2);
-
-        if(Data[i].inOut === "in" )  {
-            span2.textContent = `+ ${price}원`
-            span2.style.color = "#008000";
-
-          }  
-          else if (Data[i].inOut === "out") {
-            span2.textContent = `- ${price}원`
-          }
-
-        if (Data[i].inOut === "in") {
-            sum = sum + price;
-        } if (Data[i].inOut === "out") {
-            sum = sum - price;
-        }
-    
-    }
-       spendingText.textContent = `${sum}   지출`
- }
+//  const getDatatoDate = (obj, date) => {
+//     return obj.filter((element) => {
+//       return element.date == date;
+//     })
+//   };
+//   function printData(obj) {
+//     const uniqueDate = [...new Set(obj.map((element) => element.date))];
+//     for(let i =0; i < uniqueDate.length; i++){
+//       let selectDate = uniqueDate[i];
+//       let selectObj = getDatatoDate(obj, uniqueDate[i]);
+//       for(const elem of selectObj){
+//         console.log(elem);
+//       }
+//       console.log("하루끝!")
+//     }
  
 
 
-
-function yesterDayList(Data) {
-
-    const UL = document.querySelector('.list')
-    const spendingText = document.querySelector('.history_spending')
-    let sum = 0;
-
-    for (let i = 0; i < Data.length; i++) {
-
-
-        const creLi = document.createElement('LI');
-        creLi.className = "creLi";
-        const span1 = document.createElement("SPAN");
-        const span2 = document.createElement("SPAN");
-        span1.textContent = Data[i].item
-        // span2.textContent = Data[i].price
-        let price = Data[i].price;
-        creLi.style.borderBottom = "1px solid #dcdcdc";
-        creLi.style.padding = "3px 17px"
-        creLi.style.fontSize = "18px"
-        creLi.style.fontWeight = "300";
-        creLi.style.lineHeight = "27px";
-        history.appendChild(creLi);
-        creLi.appendChild(span1);
-        creLi.appendChild(span2);
-
-        if(Data[i].inOut === "in" )  {
-          span2.textContent = `+ ${price}원`
-          span2.style.color = "#008000";
-       
-        }  
-        else if (Data[i].inOut === "out") {
-          span2.textContent = `- ${price}원`
- 
-        }
-
-        // 수입, 지출 구분 
-
-        if (Data[i].inOut === "in") {
-            sum = sum + price;
-        } if (Data[i].inOut === "out") {
-            sum = sum - price;
-        }
-
-        //수입 지출 계산 
-    
-    }
-       spendingText.textContent = `${sum}   지출`
- }
- 
-
-
-//  드랍다운 
+//  드랍ㄹ다운 
 
 const main = document.querySelector(".bankApp")
 const dropMenu = document.querySelector('.scroll');
